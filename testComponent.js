049890065224850606615df8ddd54e0c46489278
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2021-05-18 20:11:02 +0100

    Create testComponent.js

---
+++ testComponent.js
@@ -0,0 +1,45 @@
+// function Greeting() {
+//   return <h4>Hello James</h4>;
+// }
+
+//stateless functional component
+//always return jsx
+
+// const Greeting = () => {
+//   return (
+//     <div>
+//       <h1>Hello World </h1>
+//     </div>
+//   );
+// };
+
+// const Greeting = () => {
+//   return React.createElement("div", {}, React.createElement("h1", {}, "hello"));
+// };
+
+// const Greeting = () => {
+//   return (
+//     <React.Fragment>
+//       <section onClick={() => {alert("Hello")}}>
+//         <article className='hello'>
+//           <h1>Hello World</h1>
+//           <p>How do</p>
+//         </article>
+//       </section>
+//     </React.Fragment>
+//   );
+// };
+
+// function BookList() {
+//   return (
+//     <section className="Favorite">
+//       <Person />
+//       <Message />
+//     </section>
+//   );
+// }
+
+// const Person = () => <h4>Johnny</h4>;
+// const Message = () => {
+//   <h2>Hello To You Sir</h2>;
+// };
